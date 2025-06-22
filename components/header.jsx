80d93469cd551124e3c2e7d1aaee11b936b67a36
Harshitha Shetty <141444342+HarshithaShetty27@users.noreply.github.com>
2025-06-22 23:16:59 +0530

    shadcn setup, clerk setup and logo

---
+++ components/header.jsx
@@ -0,0 +1,38 @@
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
+import Image from 'next/image'
+import Link from 'next/link'
+import { Button } from './ui/button'
+import { LayoutDashboard } from 'lucide-react'
+
+const Header = () => {
+    return (
+        <header>
+            <nav>
+                <Link href="/">
+                    <Image src="/logo.png" alt='logo' width={200} height={60} className='h-12 py-1 w-auto object-contain' />
+                </Link>
+
+                <div>
+                    <SignedIn>
+                        <Link href="/dashboard">
+                            <Button>
+                                <LayoutDashboard className='h-4 w-4' />
+                                Industry Insights
+                            </Button>
+                        </Link>
+                    </SignedIn>
+                </div>
+            </nav>
+
+            <SignedOut>
+                <SignInButton />
+                <SignUpButton />
+            </SignedOut>
+            <SignedIn>
+                <UserButton />
+            </SignedIn>
+        </header>
+    )
+}
+
+export default Header
